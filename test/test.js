10c5d670beaadf6fc48049e7e4e6daa21574c59f
Shogo Sensui <shogosensui@gmail.com>
2018-10-31 19:31:05 +0900

    Require Node.js 6 (#18)

--- test/test.js
+++ test/test.js
@@ -4,22 +4,19 @@ const path = require('path');
 const test = require('ava');
 const execa = require('execa');
 const tempy = require('tempy');
-const BinBuild = require('bin-build');
+const binBuild = require('bin-build');
 const binCheck = require('bin-check');
 const compareSize = require('compare-size');
 const jpegoptim = require('..');
 
-test.cb('rebuild the jpegoptim binaries', t => {
+test('rebuild the jpegoptim binaries', async t => {
 	const tmp = tempy.directory();
-	new BinBuild()
-		.src('https://github.com/tjko/jpegoptim/archive/RELEASE.1.4.4.tar.gz')
-		.cmd(`./configure --prefix="${tmp}" --bindir="${tmp}"`)
-		.cmd('make install')
-		.run(err => {
-			t.ifError(err);
-			t.true(fs.existsSync(path.join(tmp, 'jpegoptim')));
-			t.end();
-		});
+	await binBuild.url('https://github.com/tjko/jpegoptim/archive/RELEASE.1.4.4.tar.gz', [
+		`./configure --prefix="${tmp}" --bindir="${tmp}"`,
+		'make install'
+	]);
+
+	t.true(fs.existsSync(path.join(tmp, 'jpegoptim')));
 });
 
 test('return path to binary and verify that it is working', async t => {
