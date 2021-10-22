69e727899f2d821b752f767cd3450c7c48d38c6b
1000ch <shogosensui@gmail.com>
2021-10-22 23:20:13 +0900

    Use native ESM

--- test/test.js
+++ test/test.js
@@ -1,13 +1,14 @@
-'use strict';
-const fs = require('fs');
-const path = require('path');
-const test = require('ava');
-const execa = require('execa');
-const tempy = require('tempy');
-const binBuild = require('bin-build');
-const binCheck = require('bin-check');
-const compareSize = require('compare-size');
-const jpegoptim = require('..');
+import fs from 'node:fs';
+import path from 'node:path';
+import process from 'node:process';
+import {fileURLToPath} from 'node:url';
+import test from 'ava';
+import execa from 'execa';
+import tempy from 'tempy';
+import binBuild from 'bin-build';
+import binCheck from 'bin-check';
+import compareSize from 'compare-size';
+import jpegoptim from '../index.js';
 
 test('rebuild the jpegoptim binaries', async t => {
 	// Skip the test on Windows
@@ -19,7 +20,7 @@ test('rebuild the jpegoptim binaries', async t => {
 	const temporary = tempy.directory();
 	await binBuild.url('https://github.com/tjko/jpegoptim/archive/RELEASE.1.4.6.tar.gz', [
 		`./configure --prefix="${temporary}" --bindir="${temporary}"`,
-		'make install'
+		'make install',
 	]);
 
 	t.true(fs.existsSync(path.join(temporary, 'jpegoptim')));
@@ -31,13 +32,13 @@ test('return path to binary and verify that it is working', async t => {
 
 test('minify a JPG', async t => {
 	const temporary = tempy.directory();
-	const src = path.join(__dirname, 'fixtures/test.jpg');
+	const src = fileURLToPath(new URL('fixtures/test.jpg', import.meta.url));
 	const dest = path.join(temporary, 'test.jpg');
 	const args = [
 		'--strip-all',
 		'--all-progressive',
 		'--dest=' + temporary,
-		src
+		src,
 	];
 
 	await execa(jpegoptim, args);
